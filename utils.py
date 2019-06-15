032f8b8b3eb0b210fa7541b76618646da0b36511
Jy <yjyao1112@gmail.com>
2019-06-15 01:14:33 -0700

    Add button solver.

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 from functools import lru_cache
+import re
 
 @lru_cache(100)
 def num(name):
   return int(input(f'Number of {name}: '))
+
+def yesno(prompt):
+  return re.match('yes|y', input(f'{prompt} [Y/n] ').lower()) is not None
 
 coded_colors = {
   'r': 'red',
