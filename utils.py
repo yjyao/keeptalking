7553c97e9aff6d3edb3536f50603eb5aff4dab97
Jy <yjyao1112@gmail.com>
2019-06-17 00:54:43 -0700

    Move serial_ends_with_odd() to utils.

--- utils.py
+++ utils.py
@@ -5,6 +5,10 @@ import re
 def num(name):
   return int(input(f'Number of {name}: '))
 
+@lru_cache(1)
+def serial_ends_with_odd():
+  return int(input(f'Last digit of serial number: ')) % 2 != 0
+
 def yesno(prompt):
   return re.match('yes|y', input(f'{prompt} [Y/n] ').lower()) is not None
 
